2e9a9430eb6854ac28b446b11781987b3c5fc131
Olashina <69033389+olashina201@users.noreply.github.com>
2022-06-02 23:46:47 +0100

    try

--- hype.js
+++ hype.js
@@ -1,1 +1,17 @@
-let name = document.getElementById()
+let name = document.getElementById("fullname").value;
+let dob = document.getElementById("dob").value;
+let age = document.getElementById("age").value;
+
+let headache = document.getElementById("headache").value;
+let vision = document.getElementById("vision").value;
+let chest = document.getElementById("chest").value;
+let heartbeat = document.getElementById("heartbeat").value;
+let dizzy = document.getElementById("dizzy").value;
+let sleep = document.getElementById("sleep").value;
+
+let btn = document.getElementById("result-btn");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+  console.log("dob", dob);
+});

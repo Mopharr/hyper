4f4f96382276801e34efb494150ebae890e00f44
Olashina <69033389+olashina201@users.noreply.github.com>
2022-06-03 11:06:58 +0100

    get valuues

--- hype.js
+++ hype.js
@@ -1,17 +1,17 @@
-let name = document.getElementById("fullname").value;
-let dob = document.getElementById("dob").value;
-let age = document.getElementById("age").value;
+let name = document.getElementById("fullname");
+let dob = document.getElementById("dob");
+let age = document.getElementById("age");
 
-let headache = document.getElementById("headache").value;
-let vision = document.getElementById("vision").value;
-let chest = document.getElementById("chest").value;
-let heartbeat = document.getElementById("heartbeat").value;
-let dizzy = document.getElementById("dizzy").value;
-let sleep = document.getElementById("sleep").value;
+let headache = document.getElementById("headache");
+let vision = document.getElementById("vision");
+let chest = document.getElementById("chest");
+let heartbeat = document.getElementById("heartbeat");
+let dizzy = document.getElementById("dizzy");
+let sleep = document.getElementById("sleep");
 
 let btn = document.getElementById("result-btn");
 
 btn.addEventListener("click", (e) => {
-    e.preventDefault();
-  console.log("dob", dob);
+  e.preventDefault();
+  
 });

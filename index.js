91a214fb9dbb1eea2ea476bfc27a60e8707fce20
quadri salawudeen <olamiquadri1@gmail.com>
2022-06-06 17:04:44 +0100

    recommendation

--- index.js
+++ index.js
@@ -17,13 +17,59 @@ const result = localStorage.getItem("result");
 const hyper = document.getElementById("lll");
 const username = document.getElementById("username");
 const display = document.getElementById("disresult");
-display.innerHTML = `${JSON.parse(result).status.toFixed(2)} %`
-username.innerHTML = `${JSON.parse(result).name}`
+display.innerHTML = `${JSON.parse(result).status.toFixed(2)} %`;
+username.innerHTML = `${JSON.parse(result).name}`;
+const rec = document.getElementById("recco");
 
-if(JSON.parse(result).status <= 0.33) {
-  hyper.innerHTML = `(You are Hypotensive)`
-} else if(JSON.parse(result).status <= 0.66 && JSON.parse(result).status > 0.33) {
-  hyper.innerHTML = `(Your BP rate is Accurate)`
+const hypo = [
+  "Decreased Dietary Salt (Sodium) Intake",
+  " Increased Dietary Potassium intake",
+  "Exercise Regularly",
+  "Weight Loss",
+  "Eat a Healthy Diet",
+  "Smoking Cessation",
+  "Reducing Alcohol Consumption",
+  "Relaxation Therapy",
+  " Reduce Stress",
+  "Monitor Blood Pressure Regularly",
+];
+const hpr = [
+  'Eat more salt (increase sodium intake)',
+  'Drink more water and stay hydrated',
+  'Do not skip meals • Wear compression stockings',
+  'Avoid Sudden position changes',
+  ' Check your blood sugar level regularly',
+  'Avoid alcoholic beverages'
+]
+const acc = [
+  "Try maintain your health Status"
+];
+
+
+
+if (JSON.parse(result).status <= 0.33) {
+  hyper.innerHTML = `(You are Hypotensive)`;
+   hpr.forEach((recc) => {
+     let li = document.createElement("li");
+     li.innerText = recc;
+     rec.appendChild(li);
+   });
+
+} else if (
+  JSON.parse(result).status <= 0.66 &&
+  JSON.parse(result).status > 0.33
+) {
+  hyper.innerHTML = `(Your BP rate is Accurate)`;
+  acc.forEach((recc) => {
+    let li = document.createElement("li");
+    li.innerText = recc;
+    rec.appendChild(li);
+  });
 } else {
-  hyper.innerHTML = `(You are Hypertensive)`
+  hyper.innerHTML = `(You are Hypertensive)`;
+   hypo.forEach((recc) => { 
+     let li = document.createElement("li");
+  li.innerText = recc;
+  rec.appendChild(li);
+   });
 }
